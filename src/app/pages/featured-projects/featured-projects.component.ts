81c2e4190ee5cab48858b3c05599872be8825106
Candela Landi <landicandela01@gmail.com>
2025-07-31 20:16:47 -0300

    Update Kamak project images with new screenshots

--- src/app/pages/featured-projects/featured-projects.component.ts
+++ src/app/pages/featured-projects/featured-projects.component.ts
@@ -18,25 +18,33 @@ interface FeaturedProject {
 export class FeaturedProjectsComponent {
   projects: FeaturedProject[] = [
     {
+      id: 3,
+      title: 'ONG Valores para la Familia',
+      description: 'Sitio web desarrollado para una organización no gubernamental dedicada a la prevención del abuso sexual infantil. El proyecto incluye un diseño moderno y responsivo con enfoque en la accesibilidad y la facilidad de navegación. La plataforma presenta información educativa, recursos de prevención y canales de contacto para familias y profesionales que trabajan con niños.',
+      images: [
+        'assets/projects-img/ong-valoresparalafamilia/FireShot Capture 047 - ONG Valores para la Familia - Prevención de Abuso Sexual Infantil -_ - [valoresparalafamilia.com].png',
+        'assets/projects-img/ong-valoresparalafamilia/Screenshot 2025-07-31 180603.png'
+      ],
+      technologies: ['Angular', 'Tailwind CSS', 'TypeScript', 'HTML5', 'CSS3'],
+      liveUrl: 'https://valoresparalafamilia.com/',
+      githubUrl: ''
+    },
+    {
       id: 2,
       title: 'Kamak Desarrollos',
       description: 'Sitio institucional desarrollado para una empresa de construcción. El sistema incluye una plataforma de gestión interna con login y autenticación mediante JWT, donde los administradores acceden a un dashboard privado para cargar, editar y eliminar proyectos (CRUD). El panel permite filtrar, buscar por nombre, gestionar información interna, imágenes y videos. Los proyectos se visualizan en un mapa interactivo integrado con Google Maps, utilizando las APIs de Google Places y Geolocation.',
       images: [
-        'assets/projects-img/kamak-web/FireShot Capture 023 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 025 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 026 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 027 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 029 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 030 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 031 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 032 - Kamak Desarrollos - [localhost].png',
-                'assets/projects-img/kamak-web/agregada.png',
-        'assets/projects-img/kamak-web/FireShot Capture 033 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 034 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 039 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 040 - Kamak Desarrollos - [localhost].png',
-        'assets/projects-img/kamak-web/FireShot Capture 041 - Kamak Desarrollos - [localhost].png',
-
+        'assets/projects-img/kamak-web/FireShot Capture 063 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 062 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 061 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 060 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 059 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 058 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 055 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 054 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 053 - Kamak Desarrollos - [kamak.com.ar].png',
+        'assets/projects-img/kamak-web/FireShot Capture 028 - Kamak Desarrollos - [localhost].png',
+        'assets/projects-img/kamak-web/FireShot Capture 023 - Kamak Desarrollos - [localhost].png'
       ],
       technologies: ['Angular', 'Tailwind', 'Angular Material', 'NestJS', 'Google Maps'],
       liveUrl: 'https://kamak.com.ar',
